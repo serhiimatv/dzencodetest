e4aa280c81859a068811dfc6163e81532b8cd5a7
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2024-09-30 01:19:53 +0300

    products-page and component

--- src/components/UserCount/UserCount.tsx
+++ src/components/UserCount/UserCount.tsx
@@ -6,7 +6,7 @@ const UserCount = () => {
   const { userCount } = useSocket();
 
   return (
-    <div className="d-flex gap-2">
+    <div className="d-flex justify-content-center align-items-center gap-2">
       <span className="badge text-bg-info">{userCount}</span>
       <span>Вкладок отрыто</span>
     </div>
---
+++ src/models/products.ts
@@ -0,0 +1,22 @@
+export interface IProducts {
+  id: number;
+  serialNumber: number;
+  isNew: number;
+  photo: string;
+  title: string;
+  type: string;
+  specification: string;
+  guarantee: IGuarantee;
+  price: IPriceEntity[];
+  order: number;
+  date: string;
+}
+export interface IGuarantee {
+  start: string;
+  end: string;
+}
+export interface IPriceEntity {
+  value: number;
+  symbol: string;
+  isDefault: number;
+}
