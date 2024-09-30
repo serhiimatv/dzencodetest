c4c4c45490536c0fe3703add6c9a46b8b417e1a6
Serhii Matviievskyi <serhii.matviievskyi@gmail.com>
2024-09-30 21:35:48 +0300

    add api routes

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,7 +1,17 @@
 import Orders from "@/components/Orders/Orders";
 import styles from "./page.module.css";
 
-const PageOrders = () => {
+export const revalidate = 0;
+
+const fetchOrders = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/orders/api/`);
+  const data = await response.json();
+  return data;
+};
+
+const PageOrders = async () => {
+  const data = await fetchOrders();
+
   return (
     <main className={`${styles["page"]}`}>
       <h1>Приходы</h1>
